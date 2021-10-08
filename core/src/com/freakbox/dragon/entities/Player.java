c8540daeb0afbd2f4fd4d9fec1a7502b53b90fb2
Leonardo Dias <leonardosvdias@gmail.com>
2021-10-08 19:47:39 -0300

    Player movement

--- core/src/com/freakbox/dragon/entities/Player.java
+++ core/src/com/freakbox/dragon/entities/Player.java
@@ -1,10 +1,16 @@
 package com.freakbox.dragon.entities;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 import com.freakbox.dragon.DragonShmupGame;
+import com.freakbox.dragon.GameConstants;
 
 public class Player extends Entity {
+
+    private float velocity = 100;
 
     private TextureRegion dragonTextureRegion;
 
@@ -15,6 +21,23 @@ public class Player extends Entity {
 
     @Override
     public void update(float delta) {
+        if (Gdx.input.isKeyPressed(Input.Keys.W))
+            this.bounds.y += velocity * delta;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.S))
+            this.bounds.y -= velocity * delta;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.A))
+            this.bounds.x -= velocity * delta;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.D))
+            this.bounds.x += velocity * delta;
+
+        this.bounds.x = MathUtils.clamp(this.bounds.x,
+                0, GameConstants.WIDTH_PIXELS - this.bounds.width);
+        this.bounds.y = MathUtils.clamp(this.bounds.y,
+                0, GameConstants.HEIGHT_PIXELS - this.bounds.height);
+
 
     }
 
