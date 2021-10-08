0eb02a95ec953969faaea41176257a458b54b6dd
Leonardo Dias <leonardosvdias@gmail.com>
2021-10-08 17:04:25 -0300

    creating LibGDX project

---
+++ desktop/src/com/freakbox/dragon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.freakbox.dragon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.freakbox.dragon.DragonShmupGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DragonShmupGame(), config);
+	}
+}
